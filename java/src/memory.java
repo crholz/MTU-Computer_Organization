9baaa6b3b9dc85169197c23b9422780633e25fe7
crholz <32336715+crholz@users.noreply.github.com>
2019-09-19 12:15:36 -0400

    bump

--- java/src/memory.java
+++ java/src/memory.java
@@ -1,11 +1,11 @@
 import java.util.*;
 
 public class memory {
-	ArrayList<Object> mem;
+	ArrayList<int[]> mem;
 	int memCap;
 	
 	public memory() {
-		this.mem = new ArrayList<Object>();
+		this.mem = new ArrayList<int[]>();
 	}
 	
 	public void create(int memoryCap) {
@@ -13,13 +13,51 @@ public class memory {
 	}
 	
 	public void set(int memAddress, int hexAmount, int[] params) {
+		int found = -1;
 		for (int i = 0; i < this.mem.size(); i++) {
 			if ((this.mem.get(i))[0] == memAddress) {
-				
+				// Get and change the memory
+				found = i;
+				break;
 			}
 		}
-		int[] test = new int[5];
-		mem.add(test);
+		
+		if (found < 0) {
+			int[] newArr = new int[17];
+			newArr[0] = memAddress;
+			for (int i = 0; i < hexAmount; i++) {
+				newArr[i + 1] = params[i];
+			}
+			
+			boolean added = false;
+			
+			for (int i = 0; i < mem.size(); i++) {
+				if (this.mem.get(i)[0] > memAddress) {
+					mem.add(i, newArr);
+					added = true;
+					break;
+				}
+			}
+			
+			if (!added)
+				mem.add(newArr);
+		}
+	}
+	
+	public String dump() {
+		String builder = "Addr\t00\t01\t02\t\03\t04\t05\t06\t07\t08\t09\t0A\t0B\t0C\t0D\t0E\t0F\n";
+		
+		for (int i = 0; i < this.mem.size(); i++) {
+			for (int j = 0; j < this.mem.get(j).length; j++) {
+				if (j == this.mem.get(j).length - 1)
+					builder = builder + Integer.toHexString(this.mem.get(i)[j]) + "\n";
+				else
+					builder = builder + Integer.toHexString(this.mem.get(i)[j]) + "\t";
+			}
+		}
+		
+		
+		return builder;
 	}
 	
 	public void reset() {
