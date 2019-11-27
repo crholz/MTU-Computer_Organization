c85873f733be6c4ff8bd7fa6e3ccefe9973bb857
crholz <32336715+crholz@users.noreply.github.com>
2019-11-27 16:08:43 -0500

    Create cache.java

---
+++ java/src/cache.java
@@ -0,0 +1,19 @@
+/*
+ * The cache class
+ * Holds all of the information for the cache
+
+ */
+/*
+ * Corbin Holz
+ * crholz
+ * Michigan Techonological University
+ * Computer Organization
+ */
+
+public class cache {
+	boolean cacheStatus;
+	
+	public cache() {
+		
+	}
+}
